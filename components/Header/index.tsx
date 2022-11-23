2935ce7ae12409ae9924b8b96c77049a4ae07527
Antoree_Developer <thind9x@gmail.com>
2022-11-23 10:22:19 +0700

    fix search \

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,6 +4,7 @@
 
 import { Link } from "react-router-dom";
 import Head from "next/head";
+import { useEffect, useState } from "react";
 interface HeaderProps {
   homePage: () => void;
   onSearch: (keyword: string) => void;
@@ -14,24 +15,59 @@ export const Header: React.FC<HeaderProps> = ({
   onSearch,
   title,
 }) => {
+  const [keyword, setKeyword] = useState("");
+  const handleSubmit = (e: any) => {
+    if (keyword) {
+      onSearch(keyword);
+      return true;
+    } else {
+      return false;
+    }
+    return false;
+    e.preventDefault();
+  };
+  useEffect(() => {
+    let  input = document.getElementById("input") as HTMLInputElement;
+    input.addEventListener("keypress", function (event) {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        let btn = document.getElementById("myButton") as HTMLButtonElement;
+        btn.click();
+      }
+    });
+  }, []);
   return (
     <div className={"header"}>
       {" "}
       <Head>
         <title>{title ? title : ""}</title>
-        <meta charSet="utf-8"/>
+        <meta charSet="utf-8" />
       </Head>
-      <div style={{ display: "flex", flexWrap: "wrap" }}>
+      <div style={{ display: "flex" }}>
         <h3 onClick={homePage} style={{ cursor: "pointer" }}>
           Photos{" "}
         </h3>
         <h3 style={{ cursor: "pointer", marginLeft: "100px" }}>
           <input
-            placeholder={"Search a photos"}
+            placeholder={"Tìm kiếm hình ảnh"}
             id={"input"}
-            onChange={(e: any) => onSearch(e?.target?.value)}
+            onChange={(e: any) => setKeyword(e?.target?.value)}
             type={"search"}
           />
+          <button
+            onClick={handleSubmit}
+            id={"myButton"}
+            style={{
+              marginLeft: "5px",
+              backgroundColor: "#4CAF50",
+              border: "none",
+              padding: " 10px",
+              cursor: "pointer",
+              borderRadius: "8px",
+            }}
+          >
+            Tìm
+          </button>
         </h3>
       </div>
     </div>
